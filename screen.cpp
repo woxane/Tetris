9d8c4955c8420e3d0df1942726908a816b8ee980
woxane <mohsen.minavand1384@gmail.com>
2024-01-21 19:28:14 +0330

    Refactor Draw function in screen.cpp

--- screen.cpp
+++ screen.cpp
@@ -1,25 +1,26 @@
 #include "tetris.hpp"
 
 void Draw(Block Board[20][10]) {
-    for (int Col = 0 ; Col <= 20 ; Col++) {
-        for (int Row = 0 ; Row <= 10 ; Row++) {
-            if (Col == 0 & Row == 0) {
+    for (int Col = 0 ; Col < 22 ; Col++) {
+        for (int Row = 0 ; Row < 12 ; Row++) {
+            if (Col == 0) {
                 std::cout << "┏━━━━━woxane/tetris━━┓";
                 break; 
 
-            } else if (Col == 20 & Row == 0) {
+            } else if (Col == 21) {
                 std::cout << "┗━━━━━━━━━━━━━━━━━━━━┛";
                 break; 
-            }
 
-            if (Row == 0 || Row == 20) {
+            } else if (Row == 0 || Row == 11) {
                 std::cout << "┃";
-                continue;  
-            }
 
-            if (Board[Col - 1][Row - 1].Shape != 0 ) {
-                std::cout << "█";
+            } else if (Board[Col - 1][Row - 1].Shape != 0 ) {
+                std::cout << "██";
+            } else {
+                std::cout << "  " ;  
+
             }
         }
+        std::cout << std::endl ; 
     }
 }

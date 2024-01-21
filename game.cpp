0e7e0c06b89fa072041712b17e8ce3f4f68954f6
woxane <mohsen.minavand1384@gmail.com>
2024-01-21 21:29:17 +0330

    Create game.cpp and Implement Drop function

--- game.cpp
+++ game.cpp
@@ -1,1 +1,17 @@
 #include "tetris.hpp"
+
+std::vector<std::vector<int>> DropingBlock(Block Board[20][10]) {
+    std::vector<std::vector<int>> Cords; 
+    for (int Col = 0 ; Col < 20 ; Col++) {
+        for (int Row = 0 ; Row < 10 ; Row++) {
+            if (Board[Col][Row].Dropping) {
+                std::vector<int> Cord = {Col , Row};
+                Cords.push_back(Cord);
+            }
+        }
+    }
+    std::sort(Cords.begin() , Cords.end() , Compare);
+
+    return Cords;      
+
+}

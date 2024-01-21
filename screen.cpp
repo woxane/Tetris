4ec42b514fb2bf2e3ca06b679646fcc8d39dc6cd
woxane <mohsen.minavand1384@gmail.com>
2024-01-21 18:53:57 +0330

    Implement Draw function in screen.cpp

--- screen.cpp
+++ screen.cpp
@@ -1,1 +1,25 @@
 #include "tetris.hpp"
+
+void Draw(Block Board[20][10]) {
+    for (int Col = 0 ; Col <= 20 ; Col++) {
+        for (int Row = 0 ; Row <= 10 ; Row++) {
+            if (Col == 0 & Row == 0) {
+                std::cout << "┏━━━━━woxane/tetris━━┓";
+                break; 
+
+            } else if (Col == 20 & Row == 0) {
+                std::cout << "┗━━━━━━━━━━━━━━━━━━━━┛";
+                break; 
+            }
+
+            if (Row == 0 || Row == 20) {
+                std::cout << "┃";
+                continue;  
+            }
+
+            if (Board[Col][Row].Shape != 0 ) {
+                std::cout << "█";
+            }
+        }
+    }
+}

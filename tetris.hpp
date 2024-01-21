e9ce74c8bd7941a1fedeedd59d7f63fdc991083a
woxane <mohsen.minavand1384@gmail.com>
2024-01-21 18:25:55 +0330

    Declare Block and Game structures in Tetris.hpp

--- tetris.hpp
+++ tetris.hpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include <termios.h>
 #include <unistd.h>
+#include <vector>
 
 
 char Getch();
 void Cls();
+
+struct Block {
+    // Between 0 , 6  
+    int Shape;
+    bool Dropping; 
+
+};
+
+struct Game {
+    int FPS;
+    Block Board[10][20];
+
+};

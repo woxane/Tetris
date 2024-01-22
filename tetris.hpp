02c626b4c5953a943d89ccd4985c1537be7e5026
woxane <mohsen.minavand1384@gmail.com>
2024-01-22 18:04:59 +0330

    Declare GameOver function in tetris.hpp

--- tetris.hpp
+++ tetris.hpp
@@ -27,6 +27,7 @@ bool Compare(const std::vector<int>& a, const std::vector<int>& b);
 void Play(Game game);
 std::pair<std::vector<std::vector<int>> , std::vector<std::vector<int>>> DroppingBlock(Block Board[20][10]);
 void Drop(Block Board[20][10] , bool& CannotMove);
+bool GameOver(Block Board[20][10]);
 
 // screen.cpp Part :
 void Draw(Block Board[20][10]); 

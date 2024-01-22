2fa04901d8bdb3eecf56d081bb5a5dab5a5c7e8c
woxane <mohsen.minavand1384@gmail.com>
2024-01-22 18:28:20 +0330

    Declare RandomShape function in tetris.hpp

--- tetris.hpp
+++ tetris.hpp
@@ -2,6 +2,7 @@
 #include <termios.h>
 #include <unistd.h>
 #include <algorithm>
+#include <random>
 
 
 struct Block {
@@ -28,6 +29,7 @@ void Play(Game game);
 std::pair<std::vector<std::vector<int>> , std::vector<std::vector<int>>> DroppingBlock(Block Board[20][10]);
 void Drop(Block Board[20][10] , bool& CannotMove);
 bool GameOver(Block Board[20][10]);
+int RandomShape();
 
 // screen.cpp Part :
 void Draw(Block Board[20][10]); 

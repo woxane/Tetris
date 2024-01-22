bd557be0983939bd6baa8074c82598d24419c797
woxane <mohsen.minavand1384@gmail.com>
2024-01-22 22:12:08 +0330

    Declare ShapeCords in tetris.hpp

--- tetris.hpp
+++ tetris.hpp
@@ -17,6 +17,30 @@ struct Game {
     bool CannotMove = false; 
     bool NewShape = true;
     bool GameOver = false;
+    std::vector<std::vector<std::vector<int>>> ShapeCords = {
+        { // O Block 
+            {0,4} , {0,5} , {1,4} , {1,5}
+        },
+        { // Long Block
+            {0,3} , {0,4} , {0,5} , {0,6}
+        },
+        { // L Block
+            {0,4} , {1,4} , {2,4} , {3,4} , {0,5}
+        },
+        { // J Block 
+            {0,4} , {0,5} , {1,5} , {2,5} , {3,5} 
+        },
+        { // Zag Block
+            {0,5} , {1,5} , {1,4} , {2,4} 
+        },
+        { // Zig Block 
+            {0,4} , {1,4} , {1,5} , {2,5} 
+        },
+        { // T Block
+            {0,5} , {1,4} , {1,5} , {1,6}
+        }
+
+    };
     Block Board[20][10];
 
 };

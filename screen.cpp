9abc29255261cf80eaf80ac0240d79d502eb9631
woxane <mohsen.minavand1384@gmail.com>
2024-01-25 17:12:04 +0330

    Refactor Draw function to make pieces look colorful

--- screen.cpp
+++ screen.cpp
@@ -1,5 +1,14 @@
 #include "tetris.hpp"
 
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define CYAN    "\033[36m"
+#define ORANGE  "\033[38;2;255;165;0m"
+#define PURPLE  "\033[38;2;128;0;128m"
+
 void Draw(Block Board[18][10]) {
     for (int Col = 0 ; Col < 20 ; Col++) {
         for (int Row = 0 ; Row < 12 ; Row++) {
@@ -15,7 +24,38 @@ void Draw(Block Board[18][10]) {
                 std::cout << "┃";
 
             } else if (Board[Col - 1][Row - 1].Shape != 0 ) {
-                std::cout << "██";
+                switch (Board[Col - 1][Row - 1].Shape) {
+                    case 1 : 
+                        std::cout << YELLOW;
+                        break;
+
+                    case 2 : 
+                        std::cout << CYAN;
+                        break;
+
+                    case 3 :
+                        std::cout << BLUE;
+                        break;
+
+                    case 4 :
+                        std::cout << ORANGE;
+                        break;
+
+                    case 5 : 
+                        std::cout << RED;
+                        break;
+
+                    case 6 : 
+                        std::cout << GREEN;
+                        break;
+
+                    case 7 :
+                        std::cout << PURPLE; 
+                        break;
+                }
+
+                std::cout << "██" << RESET;
+
             } else {
                 std::cout << "  " ;  
 
